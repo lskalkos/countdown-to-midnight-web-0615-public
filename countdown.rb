55798d94fd02735f766bfecbdfe0a3aa023e8a3b
Lia Skalkos <hello@liaskalkos.net>
2015-06-03 15:41:30 -0400

    Everything working

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(count)
+  while count >= 0
+    if count == 0
+     return "0 left. HAPPY NEW YEAR!"
+    end
+   print "#{count} SECOND(S)!\n"
+    count -= 1
+  end
+end
+
+def countdown_with_sleep(count)
+  while count >= 0
+    if count == 0
+      return "0 left. HAPPY NEW YEAR!"
+    end
+    print "#{count} SECOND(S)!\n"
+    count -= 1
+    sleep(1)
+  end
 end
